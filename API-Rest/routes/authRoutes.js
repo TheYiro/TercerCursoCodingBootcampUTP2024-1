85690c02cae5c0906336abb25206c2370c8ede26
tamayo02 <juan.tamayo3@utp.edu.com>
2024-05-29 15:53:25 -0500

    cambioTamayo

--- API-Rest/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-router.post('/logout', authController.logout);
-
-module.exports = router;
