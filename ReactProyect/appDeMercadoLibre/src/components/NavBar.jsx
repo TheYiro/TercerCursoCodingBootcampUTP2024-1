95ac942ce8524a8eb326c8ad6b60686d1a5dd5f0
Joaco <71678926+TheYiro@users.noreply.github.com>
2024-07-12 14:31:00 -0500

    Update NavBar.jsx

--- ReactProyect/appDeMercadoLibre/src/components/NavBar.jsx
+++ ReactProyect/appDeMercadoLibre/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
     return (
         <div className="navBar">
-        <Link to='/'><img src="/src/assets/joacodevnobackground.png" alt="Joaco Logo" /></Link>
+        <Link to='/'><img src="https://i.ibb.co/pRqbRmy/IMG-20240603-233824-1.webp" alt="Joaco Logo" /></Link>
         </div>
         );
 }
